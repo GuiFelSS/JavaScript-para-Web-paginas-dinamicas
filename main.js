49d0453f08f2532e5b462f32319e83cedc6736dd
GuiFelSS <guilhermeflp6@gmail.com>
2024-06-17 00:49:08 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomPom () {
+    
+    document.querySelector('#som_tecla_pom').play()
+
+}
+
+function tocaSomClap () {
+jkbhjvl
+    document.querySelector('#som_tecla_clap').play()
+
+}
+
+
+
+document.querySelector('.tecla_pom');
