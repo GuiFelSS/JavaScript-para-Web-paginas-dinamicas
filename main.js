692c35ca7327eed8c03d38bcdba8c9defe285800
GuiFelSS <guilhermeflp6@gmail.com>
2024-06-23 19:26:09 -0300

    finalizando projeto
    conhecimentos estudados e adicionados no projeto:
    função anônima ;
    let & const
    lista de elementos
    requisição e extração de elementos do HTML

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-function tocaSomPom () {
-    
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else{
+        console.log("Elemento não encontado ou seletor inválido");
+    }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const intrumento = tecla.classList[1];
+
+    const idAudio = `#som_${intrumento}`;
+    // console.log(idAudio)
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+    // console.log(contador);
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter' ) {  
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
